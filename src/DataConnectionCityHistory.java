71edeee197bc95b0e6ad07aa644c05c47966edd9
oussama <ou.charafi@gmail.com>
2024-02-25 20:13:49 +0100

    I added fonction getCityHistory

--- src/DataConnectionCityHistory.java
+++ src/DataConnectionCityHistory.java
@@ -14,5 +14,24 @@ public class DataConnectionCityHistory {
     public static Connection getConnection() throws SQLException {
         return connection();
     }
+    public static List<CityHistory> getCityHistory() throws SQLException{
+        List<CityHistory> cityHistories = new ArrayList<>();
+        String sql = "SELECT * FROM cityhistory";
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet r = statement.executeQuery();
+        while (r.next()) {
+            Integer ID = r.getInt("historicalDataId");
+            Integer IDC = r.getInt("cityId");
+            Date date = r.getDate("date");
+            Integer tmp = r.getInt("temperature");
+            String name = r.getString("cityName");
+            cityHistories.add(new CityHistory(ID,  IDC, tmp ,date , name));
+        }
+        connection.close();
+        statement.close();
+        r.close();
+        return cityHistories;
+    }
 
 }
