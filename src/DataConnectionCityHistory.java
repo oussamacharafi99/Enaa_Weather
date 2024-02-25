0d2ad1652da660590a8da1a69fe594a59253afce
oussama <ou.charafi@gmail.com>
2024-02-25 20:16:21 +0100

    I added two fonctions search and modify

--- src/DataConnectionCityHistory.java
+++ src/DataConnectionCityHistory.java
@@ -46,4 +46,46 @@ public class DataConnectionCityHistory {
         System.out.println("The CityH Add successfully!");
     }
 
+
+    public static void searchCityHistory(String cityName) throws SQLException {
+        String sql = "SELECT ch.historicalDataId, ch.cityId, ch.date, ch.temperature, c.cityName " +
+                "FROM cityhistory ch " +
+                "JOIN city c ON ch.cityId = c.cityId " +
+                "WHERE ch.cityName = ?";;
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, cityName);
+        ResultSet r = statement.executeQuery();
+        while (r.next()){
+            Integer ID = r.getInt("historicalDataId");
+            Integer IDC = r.getInt("cityId");
+            Date DATE = r.getDate("date");
+            Integer TMP = r.getInt("temperature");
+            String NAME = r.getString("cityName");
+
+            System.out.println("----------------------------");
+            System.out.println("ID: " + ID);
+            System.out.println("Nom de la ville: " + NAME);
+            System.out.println("ID de la ville: " + IDC);
+            System.out.println("Date: " + DATE);
+            System.out.println("Température: " + TMP);
+            System.out.println("-----------------------------");
+
+        }
+
+    }
+
+    public static void modifierH(CityHistory cityHistory) throws SQLException {
+
+        String sql = "UPDATE cityhistory SET cityName = ?, temperature = ? WHERE cityId = ?";
+        Connection connection = connection();
+        PreparedStatement s = connection.prepareStatement(sql);
+        s.setString(1, cityHistory.getCityName());
+        s.setInt(2, cityHistory.getTemperature());
+        s.setInt(3,cityHistory.getCityId());
+        s.executeUpdate();
+        connection.close();
+        s.close();
+        System.out.println("The City updated successfully!");
+    }
 }
